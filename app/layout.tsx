8e4999394640bc12158c5c3928617c48d3815d47
pranshu <srivastavapranshu2@gmail.com>
2024-08-30 23:01:45 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <ClerkProvider appearance={{baseTheme:dark}}>
+      <html lang="en">
+      <body className={inter.className}><ThemeProvider attribute="class" forcedTheme="dark" storageKey=""brostreamer-theme>
+      {children}
+        </ThemeProvider></body>
     </html>
+    </ClerkProvider>
   );
 }
