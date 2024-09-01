3c862cc69e7045ef8b4df96e95b6309c7048c3c4
pranshu <srivastavapranshu2@gmail.com>
2024-09-01 23:26:41 +0530

    Search Component

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -6,13 +6,41 @@ import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 export const Search = () => {
-  console.log("i am logged here");
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value) return;
+    const url = qs.stringifyUrl(
+      {
+        url: "/search",
+        query: { term: value },
+      },
+      { skipEmptyString: true }
+    );
+    router.push(url);
+  };
+
+  const handleClear = () => {
+    setValue("");
+  };
   return (
-    <form className="relative w-full lg:w-[400px] flex items-center">
+    <form
+      className="relative w-full lg:w-[400px] flex items-center"
+      onSubmit={handleSubmit}
+    >
       <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         placeholder="search"
         className="rounded-l-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
       />
+      {value && (
+        <X
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+          onClick={handleClear}
+        />
+      )}
       <Button
         type="submit"
         size="sm"
