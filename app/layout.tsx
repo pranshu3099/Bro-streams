c49587e641d3219d53a9aca6c3ca0d94d3c2c7a0
pranshu <srivastavapranshu2@gmail.com>
2024-09-04 23:25:19 +0530

    layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,12 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{baseTheme:dark}}>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-      <body className={inter.className}><ThemeProvider attribute="class" forcedTheme="dark" storageKey=""brostreamer-theme>
-      {children}
-        </ThemeProvider></body>
-    </html>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="dark"
+            storageKey=""
+            brostreamer-theme
+          >
+            <Toaster theme="light" position="bottom-center" />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
