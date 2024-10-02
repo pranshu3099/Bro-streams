2c77f25e9ae7618ec462a6edc9ee99580e177483
pranshu <srivastavapranshu2@gmail.com>
2024-10-03 00:56:54 +0530

    block guest user from the stream

--- actions/block.ts
+++ actions/block.ts
@@ -1,17 +1,34 @@
 "use server";
 
-import { blockUser, UnblockUser } from "@/lib/block-service";
+import { getSelf } from "@/lib/authservice";
 import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
+import { blockUser, UnblockUser } from "@/lib/block-service";
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
 
 export const onBlock = async (id: string) => {
-  // TODO: Adapt to disconnect liveStream
-  // TODO: Allow ability to kick the guest
+  const self = await getSelf();
 
-  const blockedUser = await blockUser(id);
-  revalidatePath("/");
-  if (blockedUser) {
-    revalidatePath(`${blockedUser.blocked.username}`);
+  let blockedUser;
+
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    //This means user is a guest
   }
+
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    //This means user is not in the room
+  }
+  revalidatePath(`/u/${self.username}/community`);
+
   return blockedUser;
 };
 
