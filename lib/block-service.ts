fea61417aeeb7029cb696bf8a7da24256524c0ee
pranshu <srivastavapranshu2@gmail.com>
2024-10-06 21:18:18 +0530

    added function to get all the blocked users

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -138,3 +138,17 @@ export const UnblockUser = async (id: string) => {
 
   return unblock;
 };
+
+export const getBlockedUsers = async () => {
+  const self = await getSelf();
+  const blockedUsers = await db.block.findMany({
+    where: {
+      blockerId: self.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return blockedUsers;
+};
